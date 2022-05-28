8b884cc6befd64e0f44e55c8e6ec568e8480642e
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2022-05-28 12:48:13 +1000

    .

--- UIScales/UIScales.cs
+++ UIScales/UIScales.cs
@@ -1,10 +1,12 @@
 ﻿using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Wish;
+
 
 namespace UIScales;
 
@@ -18,6 +20,7 @@ public class UiScales : BaseUnityPlugin
     public static ConfigEntry<float> MainMenuUiScale;
     public static ConfigEntry<float> InGameUiScale;
     public static ConfigEntry<float> ZoomLevel;
+    public static ManualLogSource Log;
 
     public void Awake()
     {
@@ -29,6 +32,10 @@ public class UiScales : BaseUnityPlugin
         MainMenuUiScale = Config.Bind<float>("Scale", "MenuUIScale", 2, "UI scale while at the main menu.");
         ZoomLevel = Config.Bind<float>("Scale", "ZoomLevel", 2, "Zoom level while in game.");
         Hi = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        Log = new ManualLogSource("Log");
+        BepInEx.Logging.Logger.Sources.Add(Log);
+
     }
 
     public static void OnDestroy()
@@ -37,51 +44,50 @@ public class UiScales : BaseUnityPlugin
     }
 
 
-    [HarmonyPatch(typeof(Player), "SetZoom")]
+    [HarmonyPatch(typeof(Player))]
+    [HarmonyPatch(nameof(Player.SetZoom))]
     public static class SetZoomPatch
     {
         public static void Prefix(ref float zoomLevel)
         {
             if (!ModEnabled.Value) return;
-            if(!ZoomModEnabled.Value) return;
+            if (!ZoomModEnabled.Value) return;
             if (zoomLevel < 1) return;
             zoomLevel = ZoomLevel.Value;
         }
 
         public static void Postfix()
         {
-
         }
     }
 
     [HarmonyPatch(typeof(AdaptiveUIScale), "LateUpdate")]
-        public static class SetUiScalePatch
+    public static class SetUiScalePatch
+    {
+        public static bool Prefix()
         {
-            public static bool Prefix()
+            return !ModEnabled.Value;
+        }
+
+        public static void Postfix(AdaptiveUIScale __instance)
+        {
+            if (!ModEnabled.Value) return;
+
+            var scaler = __instance.GetComponent<CanvasScaler>();
+            var newGameValue = InGameUiScale.Value; //get saved value
+            var newMenuValue = MainMenuUiScale.Value;
+
+            if (!UiModEnabled.Value) return;
+            if (SceneManager.GetActiveScene().name.Contains("MainMenu"))
             {
-                return !ModEnabled.Value;
+                if (newMenuValue < 0) return;
+                scaler.scaleFactor = newMenuValue;
             }
-
-            public static void Postfix(AdaptiveUIScale __instance)
+            else
             {
-                if (!ModEnabled.Value) return;
-
-                var scaler = __instance.GetComponent<CanvasScaler>();
-                var newGameValue = InGameUiScale.Value; //get saved value
-                var newMenuValue = MainMenuUiScale.Value;
-
-                if (!UiModEnabled.Value) return;
-                if (SceneManager.GetActiveScene().name.Contains("MainMenu"))
-                {
-                    if (newMenuValue < 0) return;
-                    scaler.scaleFactor = newMenuValue;
-                }
-                else
-                {
-                    if (newGameValue < 0) return;
-                    scaler.scaleFactor = newGameValue;
-                }
+                if (newGameValue < 0) return;
+                scaler.scaleFactor = newGameValue;
             }
         }
-    
+    }
 }
